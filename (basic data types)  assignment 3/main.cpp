331d7aad044b045d08f43b00d520f67a98b7cada
abd alfatah <abdyaho545@gmail.com>
2022-10-20 13:41:35 +0300

    20/10/2022    assignment to hackerrank

---
+++ (basic data types)  assignment 3/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int x;
+  unsigned long long y;
+   char z='a';
+   float v;
+   double b;
+   cin>>x>>y>>z>>v>>b;
+   printf("%d\n",x);
+   printf("%llu\n",y);
+   printf("%c\n",z);
+   printf("%0.3f\n",v);
+   printf("%0.9f\n",b);
+
+
+
+
+    return 0;
+}
---
+++ (input and output)  assignment 1/main.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+   int x,y,z,k;
+   cout<<"enter three number integers:"<<endl;
+   cin>>x>>y>>z;
+   k=x+y+z;
+   cout<<x<<" "<<y<<" "<<z<<" "<<endl;
+   cout<<k<<endl;
+    return 0;
+}
+
+
