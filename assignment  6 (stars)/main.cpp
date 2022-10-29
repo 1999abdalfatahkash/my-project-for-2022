3092acf30d5de689f347fab05c4b84a79f7f2d1d
abd alfatah <abdyaho545@gmail.com>
2022-10-29 22:54:50 +0200

    assignment      loop and stars

---
+++ assignment  6 (stars)/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   for(int i=1;i<=5;i++)
+   {
+      for(int j=4;j>=i;j--)
+      {
+
+         cout<<" ";
+
+      }
+     for(int k=1;k<=i;k++)
+
+         cout<<"*";
+    cout<<endl;
+   }
+
+  cout<<endl;
+   cout<<endl;
+     cout<<endl;
+
+
+  for(int i=1;i<=5;i++)
+   {
+      for(int j=5-i;j>=0;j--)
+      {
+
+         cout<<" ";
+
+      }
+     for(int k=1;k<=i;k++)
+
+         cout<<"* ";
+    cout<<endl;
+   }
+
+    return 0;
+}
